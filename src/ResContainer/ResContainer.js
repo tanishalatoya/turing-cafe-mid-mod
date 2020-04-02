4e088c4cb8d9e0843f99f606d7c62484f23490d0
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-02 10:58:21 -0600

    Pass deleteReservation method down as a prop to the card component

--- src/ResContainer/ResContainer.js
+++ src/ResContainer/ResContainer.js
@@ -11,6 +11,7 @@ export const ResContainer = (props) => {
       number={reservation.number}
       id={reservation.id}
       key={reservation.id}
+      deleteReservation={props.deleteReservation}
     />
   })
   return (
