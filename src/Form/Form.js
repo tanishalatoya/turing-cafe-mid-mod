4d530939d32a33ea75e630115dcf67567a396f39
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-02 10:02:18 -0600

    Create Form as a class component

---
+++ src/Form/Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+export default class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+}
+
+render() {
+  <form>
+  </form>
+}
