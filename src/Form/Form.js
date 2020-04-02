32cfba71c93dabb504cc5d24baaf54a9cd6b1946
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-02 10:16:16 -0600

    Add inputs and properties to state

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -5,12 +5,46 @@ export default class Form extends Component {
   constructor() {
     super();
     this.state = {
-
+      name: '',
+      date: '',
+      time: '',
+      number: ''
     }
   }
-}
 
-render() {
-  <form>
-  </form>
+  render() {
+    return (
+      <form>
+        <input
+        type='text'
+        placeholder='Name'
+        name='name'
+        value={this.state.name}
+        />
+        <input
+        type='text'
+        placeholder='Date (mm/yy)'
+        name='date'
+        value={this.state.name}
+        />
+        <input
+        type='text'
+        placeholder='Time'
+        name='time'
+        value={this.state.name}
+        />
+        <input
+        type='text'
+        placeholder='Number of guests'
+        name='number'
+        value={this.state.name}
+        />
+        <button
+          type='button'
+        >Make Reservation
+        </button>
+      </form>
+    )
+
+  }
 }
